263bf7d0f9e248eca51e19f4c085cf13d7ea263f
Viktor Prutyanov <vitteran@gmail.com>
2014-10-01 17:21:40 +0400

    Made some comment and assert changes

--- prutyanov/quadratic-equation-solver/quadratic-equation-solver/main.c
+++ prutyanov/quadratic-equation-solver/quadratic-equation-solver/main.c
@@ -2,41 +2,43 @@
 #include <stdio.h>
 #include <float.h>
 #include <stdlib.h>
+#include <assert.h>
 
-//Equation solving
+typedef enum result_t {ONE_ROOT = 1, INFINITY_SET = -1, NO_ROOTS = 0, TWO_ROOTS = 2, INVALID_EQUATION = -2, NULL_PTR_ARGUMENTS = -3};
 
-typedef struct roots_t roots_t;
-
-struct roots_t ///Struct that describes pair of roots 
+typedef struct roots_t ///Struct that describes pair of roots 
 {
     double x1; ///First root
     double x2; ///Second root
 };
 
-const int ONE_ROOT = 1, INFINITY_SET = -1, NO_ROOTS = 0, TWO_ROOTS = 2, INVALID_EQUATION = -2, NULL_PTR_ARGUMENTS = -3;
-
-double discriminant (double a, double b, double c)
-{
 /**
-*    @param a is first coefficient
-*    @param b is second coefficient
-*    @param c is third coefficient
+*   Auxillary function that calculates discriminant.
+*   @param a is first double coefficient
+*   @param b is second double coefficient
+*   @param c is third double coefficient
+*   @return double discriminant
 */
-    return b * b - 4.0 * a * c;
-}
+double discriminant (double a, double b, double c);
 
-int solve_equation (double *coeffs, int n_coeffs, roots_t *roots, double epsilon)
-{
 /**
-*    @param *coeffs is an array of coefficients 
-*    @param *roots is a pointer to struct of roots 
-*    @param n_coeffs is number of coefficients
-*    @param epsilon describes accuracy of calculations
+*   An equation solving function.   
+*   @param *coeffs is an array of double that describes coefficients
+*   @param *roots is a pointer to roots_t (struct of roots) 
+*   @param n_coeffs is integer number of coefficients
+*   @param epsilon is double value that describes accuracy of calculations
 */
+result_t solve_equation (double *coeffs, int n_coeffs, roots_t *roots, double epsilon);
+
+
+result_t solve_equation (double *coeffs, int n_coeffs, roots_t *roots, double epsilon)
+{
+
     if ((coeffs == nullptr) || (roots == nullptr)) return NULL_PTR_ARGUMENTS;
 
     for (int i = 3; i < n_coeffs; i++) //If there exist coefficients roots with a degree higher than 2 then the equation can not be solved by this function
     {
+        assert (0 <= i && i <= n_coeffs);
         if (coeffs[i] != 0)
         {
             return INVALID_EQUATION;
@@ -91,11 +93,17 @@ int solve_equation (double *coeffs, int n_coeffs, roots_t *roots, double epsilon
     else //Quadratic equation
     {
         double d = discriminant (a, b, c);
-        if (d >= 0)
+        if (d > epsilon)
         {
             roots->x1 = (-b + sqrt (d)) / (a * 2.0);
             roots->x2 = (-b - sqrt (d)) / (a * 2.0); //Two roots
             return TWO_ROOTS;
+        }
+        else if (-epsilon <= d && d <= epsilon)
+        {
+            roots->x1 = (-b) / (a * 2.0);
+            roots->x2 = (-b) / (a * 2.0); //Two equivalent roots
+            return ONE_ROOT;
         }
         else
         {
@@ -104,19 +112,28 @@ int solve_equation (double *coeffs, int n_coeffs, roots_t *roots, double epsilon
     }
 }
 
+double discriminant (double a, double b, double c)
+{
+
+    return b * b - 4.0 * a * c;
+}
+
 int main (void)
 {
     int status = 0, n_coeffs = 0;
-    double eps = 0, *coeffs;
+    double eps = 0;
     roots_t roots = { };
 
+    printf ("Welcome to quadratic and linear equation solver!\n");
     printf ("Enter number of coefficients:\n");
     scanf("%d", &n_coeffs);
-    coeffs = (double *)calloc (n_coeffs, sizeof(*coeffs));
+    double *coeffs = (double *)calloc (n_coeffs, sizeof(*coeffs));
+    assert (coeffs != nullptr);
 
     printf("Enter coefficients:\n");
     for (int i = n_coeffs - 1; i >= 0; i--)
     {
+        assert (0 <= i && i <= n_coeffs);
         scanf("%lf",  &coeffs[i]);
     }
 
@@ -151,7 +168,10 @@ int main (void)
         default:
             break;
     }
+    
+    #ifdef _DEBUG
+        system("pause");
+    #endif
 
-    system("pause");
     return 0;
 }
