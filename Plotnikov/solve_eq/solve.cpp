9cc20542336551faf9f06d71b089b35c68fd401f
Plotnikov <akim.plotnikov@phystech.edu>
2014-10-07 03:10:08 +0400

    Исправлен баг с делением на 0 в случае когда 2-й коэфицент равен 0. Убрана функция getch. Исправлен вывод при отрицательном дискриминанте. Учтены ошибки разметки кода. Причины такой огромной табуляции мне не известны, моя среда показывает 4 пробела.

--- Plotnikov/solve_eq/solve.cpp
+++ Plotnikov/solve_eq/solve.cpp
@@ -1,26 +1,29 @@
+//#include <stdafx.h>
 #include <stdio.h>
 #include <math.h>
-#include <conio.h>
 
+//#define scanf scanf_s
 
 int main()
 {
 	printf("Enter 3 doubles: ");
 
-	double a=0,b=0,c=0;
+	double a = 0, b = 0, c = 0;
 	scanf("%lg %lg %lg", &a, &b, &c);
 
-	if (a == 0) {printf("%d", c/b);}
-	else {
+	if ((a == 0) && (b == 0)) { printf("%lg", c); }
+	else 
+		if (a == 0) { printf("%d", c / b); }
+		else {
 		double d = 0;
-		d = b*b-4*a*c;
-		if (d<0) {printf("Error");}
+		d = b*b - 4 * a*c;
+		if (d < 0) { printf("No real roots"); }
 		else { 
 			double x1 = 0, x2 = 0;
-			x1 = (-b-sqrt(d))/(2*a);
-			x2 = (-b+sqrt(d))/(2*a);
-			printf("x1= %lg, x2= %lg", x1,x2);}
+			x1 = (-b - sqrt(d)) / (2 * a);
+			x2 = (-b + sqrt(d)) / (2 * a);
+			printf("x1= %lg, x2= %lg", x1, x2);	}
 	};
-	_getch();
-	return(0);
+
+	return 0;
 }
