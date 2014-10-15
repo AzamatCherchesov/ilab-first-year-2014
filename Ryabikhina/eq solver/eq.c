80f4cc273a1315c18a787da39b6e9f03daeac510
Ryabikhina <elena-ryabikhina@yandex.ru>
2014-10-15 20:26:35 +0400

    corrections added

--- Ryabikhina/eq solver/eq.c
+++ Ryabikhina/eq solver/eq.c
@@ -1,66 +1,93 @@
 # include <stdio.h>
 # include <math.h>
+# include <float.h>
 
-# define DISKR(koef1,koef2,koef3)     ( ( ( koef2 )*( koef2 ) ) - ( 4*( koef1 )*( koef3 ) ) );
 
-//function which counts two roots
-void two_roots (double koef1, double koef2, double D)
+
+//this function is a check if the discriminant is a zero
+int proverka(double a,double b, double c)
 {
-   double x1, x2, temp;
+    if (  fabs (b * b - 4 * a * c - 0.0) <= DBL_EPSILON * fmax( fabs(b * b - 4 * a *c ), fabs (0.0))  )
+        return 0;
+    else
+        return 1;
+}
 
-   x1=(-koef2+sqrt(D))/(2*koef1);
-   x2=(-koef2-sqrt(D))/(2*koef1);
-   if (x1>x2)
-   {
-     temp=x1;
-     x1=x2;
-     x2=temp;
-   };
 
-  printf("%lg %lg",x1,x2);
+//this function check if root is a zero or not
+int if_zero(double koef1, double koef2)
+{
+    if (  fabs ( - koef2 / koef1 - 0.0) <= DBL_EPSILON*fmax( fabs( - koef2 / koef1 ), fabs (0.0))  )
+        return 0;
+    else
+        return 1;
+}
+
+
+//this function counts roots of quadratic equation
+void kvadr (double a, double b, double c)
+{
+ double x1, x2;
+
+ if (proverka(a,b,c)==1)
+ {
+
+     if ((b * b - 4 * a * c)>0)
+     {
+        x1 = ( -b + sqrt(b * b - 4 * a * c )) / (2 *a ) ;
+        x2 = ( -b - sqrt(b * b - 4 * a * c )) / (2 * a) ;
+        printf ("x1= %lg x2= %lg",x1 , x2);
+     }
+     else
+        printf("equation has not any roots");
+ }
+
+ else
+ {
+     x1= -b /( 2 * a);
+     if (if_zero(b,2* a)==1)
+        printf ("x= %lg",x1);
+     else
+        printf("x= %lg",0);
+ };
 
 }
 
-//function which counts one root
-void one_root(double koef1, double koef2)
+
+//this function solves linear equation
+void lin(double b, double c)
 {
+
    double x1;
 
-   x1=(-koef2/koef1);
-   if (koef2==0) x1=0;
+   if ((b==0)&&(c==0))
+        printf(" the equation has an infinite number of roots ");
+   if ((b==0)&&(c!=0))
+        printf(" the equation has not any roots ");
+   if (b!=0)
+   {
+       x1= -c /b;
+       if (if_zero(c,b)==1)
+            printf ("x= %lg",x1);
+       else
+            printf("x= %lg",0);
+   };
 
-   printf("%lg",x1);
 }
 
-//the case of absence of roots
-void no_roots()
-{
-    printf("NO");
-}
-
-// the case of many endless roots
-void many_roots()
-{
-    printf("R");
-}
 
 int main()
 {
-  double a,b,c,D;
 
+  double a,b,c;
+
+  printf ("enter the coefficients of the equation:\n");
   scanf("%lg %lg %lg", &a, &b, &c);
   if (a!=0)
-    {
-      D = DISKR(a,b,c);
-        if (D>0) two_roots (a, b, D);
-        if (D<0) no_roots();
-        if (D==0) one_root(2*a, b);
-    };
-  if (a==0)
-    {
-       if ((b==0)&&(c==0)) many_roots();
-       if ((b==0)&&(c!=0)) no_roots();
-       if (b!=0) one_root(b,c);
-    };
+    kvadr(a,b,c);
+  else
+    lin(b,c);
+
   return 0;
+
 }
