fa55061af7e3a3162957b12774acf08f044d69c1
Plotnikov <akim.plotnikov@phystech.edu>
2014-10-23 04:37:10 +0400

    Добавлена препроцессорная проверка для вывода пояснений. Добавлена функция подсчета количества корней. Добавлена функция вывода корня с номером n. Исправлен формат вывода в случае, когда старший коэффицент равен 0. Исправлено название функции расчета дискриминанта.

--- Plotnikov/solve_eq/solve.cpp
+++ Plotnikov/solve_eq/solve.cpp
@@ -9,9 +9,7 @@
 //!
 //! @par     Programme prints square equation roots. 
 //!
-//! @note   "Error"  - this programme prints in case of scanned uncorrcect
-//!					   coeficient (sample a = 0, b = 0, c = 2). 	
-//!			"Infinity real roots"  - this programme prints 
+//! @note   "Infinity real roots"  - this programme prints 
 //!                                    in case of infinitive amount ,
 //!			"No real roots" printed in case of real roots absence. 
 //}===============================================================================
@@ -19,6 +17,12 @@
 
 #include <stdio.h>
 #include <math.h>
+
+//#define MYCH 0 
+
+#ifndef MYCH 
+#define MYCH 1
+#endif
 
 //{=================================================================================
 //! DIS - calculate the discriminant a square equation by its coefficients.
@@ -31,37 +35,84 @@
 //!
 //}=================================================================================
 
-double DIS(double a, double b, double c)
+double dis(double a, double b, double c)
 {
 	return (b * b - 4 * a * c); 
 }
 
+//{=================================================================================
+//! roots - calculate the discriminant a square equation by its coefficients.
+//!
+//! @param     a   Equation a-coefficient
+//! @param     b   Equation b-coefficient
+//! @param     c   Equation c-coefficient
+//! 
+//! @return        number of roots
+//!
+//}=================================================================================
+
+int roots(double a, double b, double c)
+{
+	if ((a == 0) && (b == 0))
+	{
+		if (c == 0) { return -1; }
+		else return 0;
+	}
+	else
+		if (a == 0) { return 1; }
+		else
+		{
+			if (dis(a, b, c) < 0) { return 0; }
+			else
+			{
+				if (dis(a, b, c) == 0) { return 1; }
+			}
+		};
+}
+
+//{=================================================================================
+//! solve - calculate the discriminant a square equation by its coefficients.
+//!
+//!	@param	   n   root number
+//! @param     a   Equation a-coefficient
+//! @param     b   Equation b-coefficient
+//! @param     c   Equation c-coefficient
+//! 
+//! @return        number of roots
+//! 
+//}=================================================================================
+
+double solve(int n, double a, double b, double c)
+{
+	if (a == 0) { return b / c; }
+	else
+	{
+		if (n == 1) { return ((-b - sqrt(dis(a, b, c))) / (2 * a)); }
+		else { return ((-b + sqrt(dis(a, b, c))) / (2 * a)); };
+	}
+}
 
 int main()
 {
-	printf("Enter 3 doubles: ");
+	
+	if (MYCH == 0) { printf("Enter 3 solve equation coefficients: "); };
 
 	double a = 0, b = 0, c = 0;  
 	scanf("%lg %lg %lg", &a, &b, &c);
 
-	if ((a == 0) && (b == 0)) 
-	{ 
-		if (c == 0) { printf("Infinity real roots\n"); } 
-		else printf("Error\n");
-	}
+	if (roots(a, b, c) == 0) { printf("No real roots\n"); }
 	else 
-		if (a == 0) { printf("%d\n", c / b); }
-		else
-		{
-			if (DIS(a, b, c) < 0) { printf("No real roots\n"); }
-			else 
+		if (roots(a, b, c) == -1) { printf("Infinity roots\n"); }
+		else 
+			if (roots(a, b, c) == 1) { printf("x = %lg\n", solve(1, a, b, c)); }
+			else
 			{
 				double x1 = 0, x2 = 0;
-				x1 = (-b - sqrt(DIS(a, b, c))) / (2 * a);
-				x2 = (-b + sqrt(DIS(a, b, c))) / (2 * a);
-				printf("x1= %lg, x2= %lg\n", x1, x2);
-			}
-		};
+				x1 = solve(1, a, b, c);
+				x2 = solve(2, a, b, c);
+				if (MYCH != 0) { printf("%lg %lg\n", x1, x2); }
+				else { printf("x1 = %lg, x2 = %lg\n", x1, x2); };
+			};
 
 	return 0;
 }
