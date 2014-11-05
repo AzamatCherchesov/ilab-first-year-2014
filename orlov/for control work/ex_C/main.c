954062d403b75a74946b064acb1a9d2ae4acdc6e
Oleg <oleg.orlov@phystech.edu>
2014-11-05 23:00:57 +0300

    this is ex C for control work, i start work in Qt, so i don't know how safe this file else.

--- orlov/for control work/ex_C/main.c
+++ orlov/for control work/ex_C/main.c
@@ -9,7 +9,7 @@ int is_square(int num){
 }
 
 void ex_C(unsigned int n){
-  
+
   int a = 0, b = 0, c = 0, d = 0;
   unsigned int sq = 0;
   for (a = 0; sq != n; a++)
@@ -17,8 +17,11 @@ void ex_C(unsigned int n){
           for (c = 0; sq != n; c++)
               for (d = 0; sq != n; d++)
                   sq = a*a + b*b + c*c + d*d;
-
-  printf ("%u %u %u %u",d*d, c*c, b*b, a*a);
+ a--;
+ b--;
+ c--;
+ d--;
+  printf ("%u %u %u %u",a*a, b*b, c*c, d*d);
 }
 //=======================================================================
 
@@ -26,9 +29,9 @@ void ex_C(unsigned int n){
 int main()
 {
   int n;
-  scanf("%i",&n);  
+  scanf("%i",&n);
   ex_C(n);
-  
-  system("PAUSE");
- return 0;   
+
+ // system("PAUSE");
+ return 0;
 }
